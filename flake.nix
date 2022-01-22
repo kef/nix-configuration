94b4b654fa680183aed57c447a612abb27b08a5e
kef <_@_>
2022-01-22 22:17:03 +1000

    _

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  #inputs.nixpkgs.url = github:NixOS/nixpkgs;
+  inputs.nixos-hardware.url = github:NixOS/nixos-hardware/master;
+
+  outputs = { self, nixpkgs, ... } @ attrs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "aarch64-linux";
+      specialArgs = attrs;
+      #inherit nixos-hardware?
+      modules = [ ./configuration.nix ];
+    };
+  };
+}
