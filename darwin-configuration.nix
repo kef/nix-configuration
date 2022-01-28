8566857b4ec7eda4959fec151869bd93103c0c7b
kef <_@_>
2022-01-29 09:27:19 +1000

    _

---
+++ darwin-configuration.nix
@@ -0,0 +1,39 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [ <home-manager/nix-darwin> ];
+
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages =
+    [ pkgs.vim
+    ];
+
+  # Use a custom configuration.nix location.
+  # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
+  # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
+
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+  # nix.package = pkgs.nix;
+
+  # Create /etc/bashrc that loads the nix-darwin environment.
+  # programs.zsh.enable = true;  # default shell on catalina
+  # programs.fish.enable = true;
+  programs.bash.enable = true;
+
+  users.users.kef = {
+    name = "kef";
+    home = "/Users/kef";
+  };
+
+  #home-manager.useUserPackages = true; # TODO Fouls up vim.
+  home-manager.useGlobalPkgs = true;
+
+  # TODO Use NIX_PATH to find this? Or locate with nix-darwin configuration?
+  home-manager.users.kef = import /Users/kef/.config/nixpkgs/home.nix;
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
+}
