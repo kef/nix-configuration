d37fb03c0ea6eaa844cc510797ab163970704539
kef <_@_>
2022-01-31 10:07:10 +1000

    _

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -37,6 +37,8 @@
     home = "/Users/kef";
   };
 
+  system.defaults.dock.autohide = true;
+
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 4;
