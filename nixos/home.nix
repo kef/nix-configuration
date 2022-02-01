d5a5c3217d5b5600d042810e626b0efbecc64137
kef <_@_>
2022-02-01 15:11:05 +1000

    _

--- nixos/home.nix
+++ nixos/home.nix
@@ -78,13 +78,13 @@ in
       eval $(dircolors ~/.nix-profile/share/LS_COLORS)
     '';
     sessionVariables = {
-      LESS_TERMCAP_mb = "$'\e[1;32m'";
-      LESS_TERMCAP_md = "$'\e[1;32m'";
-      LESS_TERMCAP_me = "$'\e[0m'";
-      LESS_TERMCAP_se = "$'\e[0m'";
-      LESS_TERMCAP_so = "$'\e[01;33m'";
-      LESS_TERMCAP_ue = "$'\e[0m'";
-      LESS_TERMCAP_us = "$'\e[1;4;31m'";
+      LESS_TERMCAP_mb = "\e[1;32m";
+      LESS_TERMCAP_md = "\e[1;32m";
+      LESS_TERMCAP_me = "\e[0m";
+      LESS_TERMCAP_se = "\e[0m";
+      LESS_TERMCAP_so = "\e[01;33m";
+      LESS_TERMCAP_ue = "\e[0m";
+      LESS_TERMCAP_us = "\e[1;4;31m";
     };
     shellAliases = {
 
