67b207252b463d0f843ca0f327275dc078cd2999
Paul O'Keeffe <paul.okeeffe@autogeneral.com.au>
2024-05-27 12:01:48 +1000

    Fix quoting.

--- nix-darwin/rebuild.sh
+++ nix-darwin/rebuild.sh
@@ -2,5 +2,5 @@
 
 dir=$(dirname "$0")
 
-#darwin-rebuild switch --flake ${dir}
-darwin-rebuild switch --flake ${dir} --recreate-lock-file
+#darwin-rebuild switch --flake "${dir}"
+darwin-rebuild switch --flake "${dir}" --recreate-lock-file
