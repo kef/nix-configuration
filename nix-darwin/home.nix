a74109b87d418d3f4486662304dc2a8e230ed057
kef <_@_>
2025-06-29 09:59:03 +1000

    Add ffmpeg-full.

--- nix-darwin/home.nix
+++ nix-darwin/home.nix
@@ -49,6 +49,7 @@ in
 
     ollama
     uv
+    ffmpeg-full
   ] ++ lib.optional pkgs.stdenv.hostPlatform.isLinux file; # NixOS only. Use macOS supplied version of file in nix-darwin.
 
   programs.direnv = {
