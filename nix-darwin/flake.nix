75da61b785008621e4f0ca7edea5a82b5cedcff4
kef <_@_>
2026-06-12 13:33:50 +1000

    Managed homebrew using nix-homebrew. Configured declarative tap management for homebrew.

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -3,38 +3,52 @@
 
   # The unstable branch is more stable than the HEAD of the master branch, since it has passed
   # a set of CI tests on Hydra.
-  inputs.nixpkgs = {
-    type = "github";
-    owner = "NixOS";
-    repo = "nixpkgs";
-    ref = "nixos-unstable";
+  inputs = {
+    nixpkgs = {
+      type = "github";
+      owner = "NixOS";
+      repo = "nixpkgs";
+      ref = "nixos-unstable";
+    };
+
+    nix-darwin = {
+      type = "github";
+      owner = "LnL7";
+      repo = "nix-darwin";
+      ref = "master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    home-manager = {
+      type = "github";
+      owner = "nix-community";
+      repo = "home-manager";
+      ref = "master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    ls-colors = {
+      type = "github";
+      owner = "trapd00r";
+      repo = "LS_COLORS";
+      ref = "master";
+      flake = false;
+    };
+
+    nix-homebrew.url = "github:zhaofengli/nix-homebrew";
+
+    # Optional: Declarative tap management
+    homebrew-core = {
+      url = "github:homebrew/homebrew-core";
+      flake = false;
+    };
+    homebrew-cask = {
+      url = "github:homebrew/homebrew-cask";
+      flake = false;
+    };
   };
 
-  inputs.nix-darwin = {
-    type = "github";
-    owner = "LnL7";
-    repo = "nix-darwin";
-    ref = "master";
-    inputs.nixpkgs.follows = "nixpkgs";
-  };
-
-  inputs.home-manager = {
-    type = "github";
-    owner = "nix-community";
-    repo = "home-manager";
-    ref = "master";
-    inputs.nixpkgs.follows = "nixpkgs";
-  };
-
-  inputs.ls-colors = {
-    type = "github";
-    owner = "trapd00r";
-    repo = "LS_COLORS";
-    ref = "master";
-    flake = false;
-  };
-
-  outputs = { self, nixpkgs, nix-darwin, home-manager, ls-colors, ... }:
+  outputs = { self, nixpkgs, nix-darwin, home-manager, ls-colors, nix-homebrew, homebrew-core, homebrew-cask, ... }:
     let
       darwinConfiguration = { location, system, user }:
         nix-darwin.lib.darwinSystem {
@@ -48,6 +62,34 @@
               home-manager.useUserPackages = false;
               home-manager.users.${user} = import ./${location}.nix;
             }
+            nix-homebrew.darwinModules.nix-homebrew
+            {
+              nix-homebrew = {
+                # Install Homebrew under the default prefix
+                enable = true;
+
+                # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+                enableRosetta = true;
+
+                # User owning the Homebrew prefix
+                user = "kef";
+
+                # Optional: Declarative tap management
+                taps = {
+                  "homebrew/homebrew-core" = homebrew-core;
+                  "homebrew/homebrew-cask" = homebrew-cask;
+                };
+
+                # Optional: Enable fully-declarative tap management
+                #
+                # With mutableTaps disabled, taps can no longer be added imperatively with `brew tap`.
+                mutableTaps = false;
+              };
+            }
+            # Optional: Align homebrew taps config with nix-homebrew
+            ({config, ...}: {
+              homebrew.taps = builtins.attrNames config.nix-homebrew.taps;
+            })
           ];
         };
     in
