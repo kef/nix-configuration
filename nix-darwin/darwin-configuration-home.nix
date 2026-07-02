1d0b81f0f89305beff42d00088bcaf5d4cf25624
kef <_@_>
2026-07-02 13:30:18 +1000

    Upgraded nix-darwin stateVersion from 4 to 5.

--- nix-darwin/darwin-configuration-home.nix
+++ nix-darwin/darwin-configuration-home.nix
@@ -1,8 +1,6 @@
 { config, pkgs, nixpkgs, ... }:
 
 {
-  ids.gids.nixbld = 350;
-
   nix.extraOptions = ''
     experimental-features = nix-command flakes
 
@@ -88,5 +86,5 @@
   # TODO Upgrade incrementally to 7 (current).
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
-  system.stateVersion = 4;
+  system.stateVersion = 5;
 }
