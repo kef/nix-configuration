5b76a3c7ca8ffedc5cc5a026164bf155c8a13e0b
kef <_@_>
2026-07-03 10:13:26 +1000

    Upgraded home-manager stateVersion from 26.05 to 26.11.

--- nix-darwin/home.nix
+++ nix-darwin/home.nix
@@ -303,5 +303,5 @@ in
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "26.05";
+  home.stateVersion = "26.11";
 }
