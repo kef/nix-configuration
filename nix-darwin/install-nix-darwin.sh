2569009374c6004a6e5d2c4006549dc556c80883
kef <_@_>
2026-08-14 17:12:42 +1000

    Use shave as host in nix-darwin fresh installation script.

--- nix-darwin/install-nix-darwin.sh
+++ nix-darwin/install-nix-darwin.sh
@@ -5,5 +5,5 @@ dir=$(dirname "$0")
 #nix run nix-darwin/master#darwin-rebuild -- switch --flake "${dir}"
 
 # Install nix-darwin on a fresh Nix installation.
-sudo nix build .#darwinConfigurations.A08758.system
+sudo nix build .#darwinConfigurations.shave.system
 sudo ./result/sw/bin/darwin-rebuild switch --flake .
