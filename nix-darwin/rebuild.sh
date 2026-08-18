6a5a32cba08b654b3400acd14abcd71ec16e0deb
kef <_@_>
2026-08-18 10:17:20 +1000

    Updated flake.

--- nix-darwin/rebuild.sh
+++ nix-darwin/rebuild.sh
@@ -3,5 +3,5 @@
 dir=$(dirname "$0")
 
 #nix flake update --flake "${dir}"
-#sudo darwin-rebuild switch --flake "${dir}" --show-trace --print-build-logs --verbose
 sudo darwin-rebuild switch --flake "${dir}"
+#sudo darwin-rebuild switch --flake "${dir}" --show-trace --print-build-logs --verbose
